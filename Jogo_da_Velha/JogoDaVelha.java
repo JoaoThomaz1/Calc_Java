3d87947bea4a22e4b93a0d734183ac20a4a8cf35
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2023-12-15 18:02:00 -0300

    segundo commit 15_12

---
+++ Jogo_da_Velha/JogoDaVelha.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class JogoDaVelha {
+   public static void main(String[] args) {
+   
+      Campo[][] velha=new Campo[3][3];
+      char simboloAtual='X';
+      Boolean game=true;
+      String vtoria="";
+      Scanner scan=new Scanner(System.in);
+
+      while(game){
+
+      }
+
+   }
+
+   public static void desenhaogo(Campo[][] velha){
+      
+   }
+
+
+
+}
