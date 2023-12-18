68b5ff2692d6ea5b68cea8626419ce73a63b4c0b
João_Thomaz <joaovitor_rodriguesthomaz@hotmail.com>
2023-12-18 10:31:42 -0300

    commit 18_12

--- Jogo_da_Velha/JogoDaVelha.java
+++ Jogo_da_Velha/JogoDaVelha.java
@@ -11,15 +11,53 @@ public class JogoDaVelha {
       Scanner scan=new Scanner(System.in);
 
       while(game){
+         desenhaogo(velha);
+         vitoria=verificaVitoria(velha)
+
+         if (!vitoria.equals("")){
+            System.out.printf("Jogador %s venceu%n", vitoria);
+            break;
+         }
+
+         try{
+
+         }catch(Exception e){
+            System.out.printf("ERRO");
+         }
+         
+         System.out.printf("Fim de Jogo!");
 
       }
 
    }
 
    public static void desenhaogo(Campo[][] velha){
-      
+      //limparTela()
+      System.out.println("        0    1     2");
+      System.out.printf("0   %c | %c | %c %n", velha[0][0].getSimbolo(), velha[0][1].getSimbolo(), velha[0][2].getSimbolo());
+      System.out.println("       ---------------");
+      System.out.printf("1   %c | %c | %c %n", velha[1][0].getSimbolo(), velha[1][1].getSimbolo(), velha[1][2].getSimbolo());      
+      System.out.println("       ---------------");
+      System.out.printf("1   %c | %c | %c %n", velha[2][0].getSimbolo(), velha[2][1].getSimbolo(), velha[2][2].getSimbolo());
    }
 
+   public static void limparTela() {
+      for (int cont=0; cont<200;cont++) {
+         System.out.println("");
+      }
+   }
 
+   public void Boolean verificarJogada(Campos[][] velha, int p[], char simboloAtual){
+      if(velha[p[0]][p[1]].getSimbolo()==''){
+         velha[p[0]][p[1]].setSimbolo(simboloAtual);
+         return true;
+      }else{
+         return false;
+      }
+   }
+
+   public static String verificaVitoria(Campo[][] velha){
+      return "";
+   }
 
 }
